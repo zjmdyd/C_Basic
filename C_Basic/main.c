5235f72ee32a74e6e12016ea91783b5d2ac41120
YunTu <yuntu@YunTudeMac-mini.local>
2015-08-29 17:37:57 +0800

    Initial Commit

---
+++ C_Basic/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C_Basic
+//
+//  Created by YunTu on 15/8/29.
+//  Copyright (c) 2015年 YunTu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
